56d5ee945ff299eb797514081b8b8db46232ac10
Bizhan <bizhanbazi31@gmail.com>
2026-04-21 10:20:52 +0100

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   server: {
-    host: true,
-    port: 5173
+    host: true
   },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Todo App',
-        short_name: 'Todo',
-        display: 'standalone',
-        theme_color: '#000000',
-        background_color: '#ffffff',
-        start_url: '/'
-      }
-    })
-  ]
+  plugins: [react()]
 })
